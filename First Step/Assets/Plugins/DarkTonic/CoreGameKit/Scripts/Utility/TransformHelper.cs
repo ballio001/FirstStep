8b1e4117a1385955bbca79edac7eb32eb247eab2
rudiger <rudiger29@live.nl>
2019-06-07 17:41:36 +0200

    Added Assets

---
+++ First Step/Assets/Plugins/DarkTonic/CoreGameKit/Scripts/Utility/TransformHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*! \cond PRIVATE */
+namespace DarkTonic.CoreGameKit {
+    public static class TransformHelper {
+        public static Vector3 GetGlobalScale(this Transform trans) {
+            var totalScale = trans.localScale;
+
+            while (true) {
+                trans = trans.parent;
+                if (trans == null) {
+                    break;
+                }
+
+                totalScale = new Vector3(trans.localScale.x * totalScale.x, trans.localScale.y * totalScale.y, trans.localScale.z * totalScale.z);
+            }
+
+            return totalScale;
+        }
+    }
+}
+/*! \endcond */
